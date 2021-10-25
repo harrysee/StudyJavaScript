ea103e492b4ac8c3c5ef5398ba7c5393608d7870
Jyuri <jeon1310@gmail.com>
2021-10-25 21:43:05 +0900

    미니게임 - javascript filter 로직 완성

--- toyProject/toyProject1.js
+++ toyProject/toyProject1.js
@@ -1,26 +1,23 @@
 'use strict';
+// 선언부
 const itemsContainer = document.querySelector('.items');
-
 const all_clothes = [
-    makeGoods('blue_p','make, large','p','blue'),
-    makeGoods('pink_s','make, small','s','pink'),
-    makeGoods('blue_s','make, big','s','blue'),
-    makeGoods('yellow_p','make, midium','p','yellow'),
-    makeGoods('pink_t','make, long','t','pink'),
-    makeGoods('blue_s','make, large','s','blue'),
-    makeGoods('pink_p','make, big','p','pink'),
-    makeGoods('yellow_s','make, midium','s','yellow'),
-    makeGoods('yellow_t','make, large','t','yellow'),
-    makeGoods('blue_p','make, big','p','blue'),
-    makeGoods('pink_s','make, large','s','pink'),
-    makeGoods('yellow_p','make, midium','p','yellow'),
+    makeGoods('blue_p', 'make, large', 'p', 'blue'),
+    makeGoods('pink_s', 'make, small', 's', 'pink'),
+    makeGoods('blue_s', 'make, big', 's', 'blue'),
+    makeGoods('yellow_p', 'make, midium', 'p', 'yellow'),
+    makeGoods('pink_t', 'make, long', 't', 'pink'),
+    makeGoods('blue_s', 'make, large', 's', 'blue'),
+    makeGoods('pink_p', 'make, big', 'p', 'pink'),
+    makeGoods('yellow_s', 'make, midium', 's', 'yellow'),
+    makeGoods('yellow_t', 'make, large', 't', 'yellow'),
+    makeGoods('blue_p', 'make, big', 'p', 'blue'),
+    makeGoods('pink_s', 'make, large', 's', 'pink'),
+    makeGoods('yellow_p', 'make, midium', 'p', 'yellow'),
 ];
-console.log(all_clothes[0].image);
-console.log(itemsContainer);
-
 // object 만들어주는 함수
-function makeGoods(src, comment, tps ,color){
-    const image = new Image();
+function makeGoods(src, comment, tps, color) {
+    const image = new Image()
     image.className = 'item__img';
     image.src = `imgs/${src}.png`;
     image.alt = `${color}-${tps}`;
@@ -31,14 +28,43 @@ function makeGoods(src, comment, tps ,color){
         comment
     };
 }
-
-for (let i = 0; i < all_clothes.length; i++) {
-    const item = document.createElement('li');
-    item.setAttribute('class','item');
-    const span = document.createElement('span');
-    span.className ='item__text';
-    span.append(all_clothes[i].comment);
-    item.appendChild(all_clothes[i].image);
-    item.appendChild(span);
-    itemsContainer.appendChild(item);
+// 처음 값 설정
+function reset(){
+    removeAll();
+    for (let i = 0; i < all_clothes.length; i++) {
+        const item = createNode(all_clothes[i].comment, all_clothes[i].image);
+        itemsContainer.appendChild(item);
+    }
 }
+const createNode = (comment, image) => {
+    const item = document.createElement('li');
+    const span = document.createElement('span');
+    span.className = 'item__text';
+    span.append(comment);
+    item.setAttribute('class', 'item');
+    item.appendChild(image);
+    item.appendChild(span);
+    return item;
+}
+reset();
+function removeAll() {
+    while (itemsContainer.hasChildNodes()) {
+        itemsContainer.removeChild(itemsContainer.firstChild);
+    }
+}
+
+function colorView(color) {
+    console.log(color);
+    removeAll();
+    const c_Collection = all_clothes.filter(clothe => clothe.color === color);
+    c_Collection.forEach(c => itemsContainer.appendChild(createNode(c.comment, c.image)));
+}
+
+function shapeView(shape) {
+    console.log(shape);
+    removeAll();
+    const s_Collection = all_clothes.filter(clothe => clothe.tps === shape);
+    s_Collection.forEach(s => itemsContainer.appendChild(createNode(s.comment, s.image)));
+}
+
+//document.getElementsByClassName('logo').onclick = reset;
