1dfe8b1faf584a4d3ac857e6d293d443e31c0baf
Jyuri <jeon1310@gmail.com>
2022-01-04 18:06:26 +0900

    json까지

--- js_reStudy/main.js
+++ js_reStudy/main.js
@@ -14,3 +14,60 @@
 
 // console.log(name)
 // console.log(window)
+
+// ----------- 배열과 오브젝트 -------
+// 문자와 숫자 섞어서 ㄱㄴ
+const animal = "lion";
+const animal2 = "ravit";
+const animals = ["lion", "rabit","monkey", "dog","cat"]
+const fruits = 'apple, banana, stroberry'
+
+console.log(animals)
+console.log(animals)
+animals[3] = "tiger"    // 배열 개수 추가
+animals.push("frog")
+animals.unshift("snake")
+animals.pop()   // 맨 마지막 지우기
+// 배열인지 확인하는 방법
+// Array.isArray(값)
+console.log(Array.isArray(animals)) // true
+console.log(Array.isArray(fruits))  // false
+
+console.log(animals.indexOf("monkey"))  // 인덱스 찾기
+
+// splice / slice
+// splice - 배열.splice(시작점, 뺄갯수, 대체할 값) 0부터 2개 빼기
+console.log(animals)
+console.log("love", animals.splice(0,2,"love"))
+console.log(animals)
+console.log(animals.splice(animals.indexOf("rabit"),3) )   
+console.log(animals)
+
+// slice - 배열.slice(시작인덱스, 끝나는인덱스)
+// 끝인덱스 제외하고 잘림
+console.log(animals.slice(0,3))
+
+
+// object : 어떤 대상의 속성:값 들로 이루어져있음
+// 인덱스 == key
+// object 값만 바꾸는 것 괜춘
+const monkey = {
+    name: "kiki",
+    age:5,
+    isCute: true,
+    food: [ 'banana', ' carret', 'potato']
+}
+
+console.log(monkey)
+monkey["size"] = "big"
+
+// json 
+// json으로 변형하기
+// json -> object 형태로 이루어져있음 ( 키:값)
+console.log(monkey)
+const monkeyJson = JSON.stringify(monkey)
+console.log(monkeyJson)
+
+// json -> object
+const parsedJson = JSON.parse(monkeyJson)
+console.log(parsedJson.name)
